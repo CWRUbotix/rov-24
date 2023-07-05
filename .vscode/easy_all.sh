bd7ae30583787a27506ebb0b67ce9800e3a6fd40
Michael Carlstrom <rmc@carlstrom.com>
2023-07-05 13:33:49 -0400

    fixed easy_all/easy_build

--- .vscode/easy_all.sh
+++ .vscode/easy_all.sh
@@ -1,4 +1,7 @@
-rosdep update --rosdistro=$ROS_DISTRO
+# Done to suppress setup.py warnings
+PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources; export PYTHONWARNINGS
+
+rosdistro=$ROS_DISTRO
 rosdep install --from-paths src --ignore-src -r -y
 # Stolen from colcon build command in VsCode
 colcon build --symlink-install
--- .vscode/easy_build.sh
+++ .vscode/easy_build.sh
@@ -1,3 +1,6 @@
+# Done to suppress setup.py warnings
+PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources; export PYTHONWARNINGS
+
 # Stolen from colcon build command in VsCode
 colcon build --symlink-install
 source install/setup.bash
