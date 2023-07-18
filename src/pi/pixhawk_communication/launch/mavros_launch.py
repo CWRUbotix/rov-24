c54a98695665bcb0b0886166d6bdc5cfb456ab28
Michael Carlstrom <rmc@carlstrom.com>
2023-07-18 19:08:33 -0400

    tested on hardware

--- src/pi/pixhawk_communication/launch/mavros_launch.py
+++ src/pi/pixhawk_communication/launch/mavros_launch.py
@@ -1,8 +1,10 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, GroupAction
+
 from launch.launch_description_sources import AnyLaunchDescriptionSource
+from launch_ros.actions import SetParameter
 
 
 def generate_launch_description():
@@ -14,9 +16,22 @@ def generate_launch_description():
             os.path.join(
                 mavros_path, 'launch', 'apm.launch'
             )
-        ]),
+        ])
+    )
+
+    # https://github.com/mavlink/mavros/issues/1632
+    # Done so RC Override thinks mavros in a gcs.
+    system_id_param = SetParameter(name="system_id", value=255)
+    component_id_param = SetParameter(name="component_id", value=240)
+
+    mavros_launch_params = GroupAction(
+        actions=[
+            system_id_param,
+            component_id_param,
+            mavros_launch
+        ]
     )
 
     return LaunchDescription([
-        mavros_launch
+        mavros_launch_params
     ])
