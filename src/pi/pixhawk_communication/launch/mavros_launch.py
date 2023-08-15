3e3ab0f28105f4d49f6cfcd2abd365deb7172916
ROV Laptop <rov@laptop.com>
2023-08-15 18:06:17 -0400

    updates

--- src/pi/pixhawk_communication/launch/mavros_launch.py
+++ src/pi/pixhawk_communication/launch/mavros_launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
             {"system_id": 255},
             # plugin_allowlist allows which mavros nodes get launched default is all of them.
             {"plugin_allowlist": ["sys_status", "rc_io", "command"]},
-            {"fcu_url", "/dev/ttyPixhawk:57600"}
+            {"fcu_url", "/dev/ttyPixhawk"}
         ],
         remappings=[('/pi/mavros/rc/override', '/mavros/rc/override'),
                     ('/pi/mavros/cmd/arming', '/mavros/cmd/arming')]
