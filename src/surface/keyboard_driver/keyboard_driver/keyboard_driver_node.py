2dae43562f330ee79259e699f836d1f7a6fcc9ab
Michael Carlstrom <rmc@carlstrom.com>
2023-08-15 21:12:15 -0400

    keyboard stuff

--- src/surface/keyboard_driver/keyboard_driver/keyboard_driver_node.py
+++ src/surface/keyboard_driver/keyboard_driver/keyboard_driver_node.py
@@ -9,48 +9,47 @@ from rclpy.qos import qos_profile_system_default
 from interfaces.msg import ROVControl
 
 
-class Keys:
-    # key bindings
-    FORWARD = "w"
-    BACKWARD = "s"
-    LEFT = "a"
-    RIGHT = "d"
-    UP = "2"
-    DOWN = "x"
+# key bindings
+FORWARD = "w"
+BACKWARD = "s"
+LEFT = "a"
+RIGHT = "d"
+UP = "2"
+DOWN = "x"
 
-    ROLL_LEFT = "j"
-    ROLL_RIGHT = "l"
-    PITCH_UP = "i"
-    PITCH_DOWN = "k"
-    YAW_LEFT = "h"
-    YAW_RIGHT = ";"
+ROLL_LEFT = "j"
+ROLL_RIGHT = "l"
+PITCH_UP = "i"
+PITCH_DOWN = "k"
+YAW_LEFT = "h"
+YAW_RIGHT = ";"
 
-    HELP = "p"
+HELP = "p"
 
-    HELP_MSG = """
-    Use keyboard to control ROV
+HELP_MSG = """
+Use keyboard to control ROV
 
-    Key Bindings:
-    [2]
-    [w]            [i]
-    [a][s][d]   [h][j][k][l][;]
-    [x]
+Key Bindings:
+[2]
+[w]            [i]
+[a][s][d]   [h][j][k][l][;]
+[x]
 
-    [w] = Forward
-    [s] = Backward
-    [a] = Left
-    [d] = Right
-    [2] = Up
-    [x] = Down
+[w] = Forward
+[s] = Backward
+[a] = Left
+[d] = Right
+[2] = Up
+[x] = Down
 
-    [j] = Roll Left
-    [l] = Roll Right
-    [i] = Pitch Up
-    [k] = Pitch Down
-    [h] = Yaw Left
-    [;] = Yaw Right
+[j] = Roll Left
+[l] = Roll Right
+[i] = Pitch Up
+[k] = Pitch Down
+[h] = Yaw Left
+[;] = Yaw Right
 
-    [p] = Show this help"""
+[p] = Show this help"""
 
 
 # Range of values Pixhawk takes
@@ -66,25 +65,21 @@ class KeyboardListenerNode(Node):
         self.pub_status = self.create_publisher(
             ROVControl, "manual_control", qos_profile_system_default
         )
-        self.logger.info(Keys.HELP_MSG)
+        self.get_logger().info(HELP_MSG)
         self.status = {
-            "forward": False,
-            "backward": False,
-            "left": False,
-            "right": False,
-            "up": False,
-            "down": False,
-            "roll_left": False,
-            "roll_right": False,
-            "pitch_up": False,
-            "pitch_down": False,
-            "yaw_left": False,
-            "yaw_right": False,
+            FORWARD: False,
+            BACKWARD: False,
+            LEFT: False,
+            RIGHT: False,
+            UP: False,
+            DOWN: False,
+            ROLL_LEFT: False,
+            ROLL_RIGHT: False,
+            PITCH_UP: False,
+            PITCH_DOWN: False,
+            YAW_LEFT: False,
+            YAW_RIGHT: False,
         }
-
-    @property
-    def logger(self):
-        return self.get_logger()
 
     def on_press(self, key: Optional[Key | KeyCode]):
         try:
@@ -98,40 +93,15 @@ class KeyboardListenerNode(Node):
             else:
                 return
 
-            match key_name:
-                case Keys.FORWARD:
-                    self.status["forward"] = True
-                case Keys.BACKWARD:
-                    self.status["backward"] = True
-                case Keys.LEFT:
-                    self.status["left"] = True
-                case Keys.RIGHT:
-                    self.status["right"] = True
-                case Keys.UP:
-                    self.status["up"] = True
-                case Keys.DOWN:
-                    self.status["down"] = True
-                case Keys.ROLL_LEFT:
-                    self.status["roll_left"] = True
-                case Keys.ROLL_RIGHT:
-                    self.status["roll_right"] = True
-                case Keys.PITCH_UP:
-                    self.status["pitch_up"] = True
-                case Keys.PITCH_DOWN:
-                    self.status["pitch_down"] = True
-                case Keys.YAW_LEFT:
-                    self.status["yaw_left"] = True
-                case Keys.YAW_RIGHT:
-                    self.status["yaw_right"] = True
-                case Keys.HELP:
-                    self.logger.info(Keys.HELP_MSG)
-                case _:
-                    return
+            if key_name == HELP:
+                self.get_logger().info(HELP_MSG)
+            else:
+                self.status[key_name] = True
 
             self.pub_rov_control()
 
         except Exception as e:
-            self.logger.error(str(e))
+            self.get_logger().error(str(e))
             raise e
 
     def on_release(self, key: Optional[Key | KeyCode]):
@@ -146,52 +116,27 @@ class KeyboardListenerNode(Node):
             else:
                 return
 
-            match key_name:
-                case Keys.FORWARD:
-                    self.status["forward"] = False
-                case Keys.BACKWARD:
-                    self.status["backward"] = False
-                case Keys.LEFT:
-                    self.status["left"] = False
-                case Keys.RIGHT:
-                    self.status["right"] = False
-                case Keys.UP:
-                    self.status["up"] = False
-                case Keys.DOWN:
-                    self.status["down"] = False
-                case Keys.ROLL_LEFT:
-                    self.status["roll_left"] = False
-                case Keys.ROLL_RIGHT:
-                    self.status["roll_right"] = False
-                case Keys.PITCH_UP:
-                    self.status["pitch_up"] = False
-                case Keys.PITCH_DOWN:
-                    self.status["pitch_down"] = False
-                case Keys.YAW_LEFT:
-                    self.status["yaw_left"] = False
-                case Keys.YAW_RIGHT:
-                    self.status["yaw_right"] = False
-                case Keys.HELP:
-                    self.logger.info(Keys.HELP_MSG)
-                case _:
-                    return
+            if key_name == HELP:
+                pass
+            else:
+                self.status[key_name] = False
 
             self.pub_rov_control()
 
         except Exception as e:
-            self.logger.error(str(e))
+            self.get_logger().error(str(e))
             raise e
 
     def pub_rov_control(self):
         msg = ROVControl()
-        msg.x = (self.status["forward"] - self.status["backward"]) * 400 + 1500
-        msg.y = (self.status["left"] - self.status["right"]) * 400 + 1500
-        msg.z = (self.status["up"] - self.status["down"]) * 400 + 1500
-        msg.roll = (self.status["roll_left"] - self.status["roll_right"]) * 400 + 1500
+        msg.x = (self.status[FORWARD] - self.status[BACKWARD]) * 400 + 1500
+        msg.y = (self.status[LEFT] - self.status[RIGHT]) * 400 + 1500
+        msg.z = (self.status[UP] - self.status[DOWN]) * 400 + 1500
+        msg.roll = (self.status[ROLL_LEFT] - self.status[ROLL_RIGHT]) * 400 + 1500
 
-        msg.pitch = (self.status["pitch_up"] - self.status["pitch_down"]) * 400 + 1500
+        msg.pitch = (self.status[PITCH_UP] - self.status[PITCH_DOWN]) * 400 + 1500
 
-        msg.yaw = (self.status["yaw_left"] - self.status["yaw_right"]) * 400 + 1500
+        msg.yaw = (self.status[YAW_LEFT] - self.status[YAW_RIGHT]) * 400 + 1500
 
         self.pub_status.publish(msg)
 
