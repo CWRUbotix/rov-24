60cd7170fca0bb2e3e561bd30ebe64f8a0cb5a65
Tyler Zupfer <tzupfer@gmail.com>
2023-09-30 16:20:26 -0400

    It's not working today <3

--- src/surface/gui/gui/widgets/flood_visual.py
+++ src/surface/gui/gui/widgets/flood_visual.py
@@ -1,45 +1,56 @@
 ## /flooding (boolean) (True is flooding)
-import rclpy
 from rov_msgs.msg import Flooding
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
-from PyQt5.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt6.QtGui import QPainter, QColor
 from gui.event_nodes.subscriber import GUIEventSubscriber
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtCore import pyqtSignal, pyqtSlot
 
 class FloodVisual(QWidget):
+    signal: pyqtSignal = pyqtSignal(Flooding)
+
     def __init__(self):
         super().__init__()
-        
-        self.signal: pyqtSignal = pyqtSignal(Flooding.flooding)
         self.signal.connect(self.refresh)
-        self.subscription: GUIEventSubscriber = GUIEventSubscriber(Flooding, '/flooding', signal, 10)
-
-        self.indicator = IndicatorWidget(self)
+        self.subscription: GUIEventSubscriber = GUIEventSubscriber(Flooding, '/flooding', self.signal, 10)
         self.layout = QVBoxLayout()
-        self.label = QLabel('Indicator')
+        self.label = QLabel('Flooding Indicator')
         self.layout.addWidget(self.label)
-        self.layout.addWidget(self.indicator)
+
+        # self.indicator = Indicator()
+        # self.layout.addWidget(self.indicator)
+        
         self.setLayout(self.layout)
 
-    def refresh(self, msg):
-        self.indicator.set_status(msg.flooding)
+    @pyqtSlot(Flooding)
+    def refresh(self, msg: Flooding):
+        self.indicator.setBooleanValue(msg.flooding)
 
-class IndicatorWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.status = False
+# class Indicator(QWidget):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#         self.setFixedSize(100, 100)
+#         self.boolean_value = False
+#         self.update() 
 
-    def set_status(self, status):
-        self.status = status
-        self.update()  # Redraw the widget
+#     def setBooleanValue(self, value):
+#         self.boolean_value = value
+#         self.update()  # Trigger a repaint when the boolean value changes
 
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-        rect = self.contentsRect()
-        color = QColor(0, 255, 0) if self.status else QColor(255, 0, 0)
+#     def update(self):
+#         painter = QPainter(self)
+#         painter.begin(self)
+#         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
-        # Draw the circular light
-        painter.setBrush(color)
-        painter.drawEllipse(rect)
-        painter.end()
+#         # Set the circle color based on the boolean value
+#         if self.boolean_value:
+#             circle_color = QColor(0, 255, 0)  # Green
+#         else:
+#             circle_color = QColor(255, 0, 0)  # Red
+
+#         # Draw the colored circle
+#         painter.setBrush(circle_color)
+#         painter.drawEllipse(self.rect())
+#         painter.end()
+
+        
+
