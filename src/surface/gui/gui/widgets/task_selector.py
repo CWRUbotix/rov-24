f92023808e73e9d757c0fa94e611d91af01c5e9b
Benjamin Poulin <90342856+benjaminwp18@users.noreply.github.com>
2023-10-14 15:42:32 -0400

    Integrate new teleop & delete task selector

--- src/surface/gui/gui/widgets/task_selector.py
+++ src/surface/gui/gui/widgets/task_selector.py
@@ -1,22 +1,18 @@
 from gui.event_nodes.client import GUIEventClient
-from gui.event_nodes.subscriber import GUIEventSubscriber
 from PyQt6.QtCore import pyqtSignal, pyqtSlot
 from PyQt6.QtWidgets import QGridLayout, QLabel, QPushButton, QWidget
-from task_selector.tasks import Tasks
 
-from rov_msgs.msg import TaskFeedback
-from rov_msgs.srv import TaskRequest
+from rov_msgs.srv import TaskControl
 
 WIDTH = 200
 
 
 class TaskSelector(QWidget):
-    """Qwidget that handles task selection with a dropdown."""
+    """QWidget that handles task selection with a dropdown."""
 
     # Declare signals with "object" params b/c we don't have access to
     # the ROS service object TaskRequest_Response
-    scheduler_response_signal: pyqtSignal = pyqtSignal(TaskRequest.Response)
-    update_task_dropdown_signal: pyqtSignal = pyqtSignal(TaskFeedback)
+    scheduler_response_signal: pyqtSignal = pyqtSignal(TaskControl.Response)
 
     def __init__(self):
         super().__init__()
@@ -25,77 +21,63 @@ class TaskSelector(QWidget):
         self.setLayout(layout)
 
         # Create Start button
-        self.start_btn = QPushButton("Auto Docking")
+        self.start_btn = QPushButton("Start Auto Docking")
         self.start_btn.clicked.connect(self.start_btn_clicked)
         self.start_btn.setFixedHeight(75)
         self.start_btn.setFixedWidth(WIDTH)
 
         # Create Stop button
-        self.stop_btn = QPushButton("Manual Control")
-        self.stop_btn.clicked.connect(self.manual_control_btn_clicked)
+        self.stop_btn = QPushButton("Stop Auto Docking")
+        self.stop_btn.clicked.connect(self.stop_btn_clicked)
         self.stop_btn.setFixedHeight(75)
         self.stop_btn.setFixedWidth(WIDTH)
 
         # Add 'Task: ' label
-        self.task_label: QLabel = QLabel()
-        self.task_label.setFixedWidth(WIDTH)
-        self.task_label.setText('Task: Manual Control')
+        self.task_status: QLabel = QLabel()
+        self.task_status.setFixedWidth(WIDTH)
+        self.task_status.setText('Auto Docking: Disabled')
 
         # Setup Grid
-        layout.addWidget(self.task_label, 1, 1, 2, 2)
+        layout.addWidget(self.task_status, 1, 1, 2, 2)
         layout.addWidget(self.start_btn, 2, 1)
         layout.addWidget(self.stop_btn, 3, 1)
 
-        # Create ROS nodes #
-        # Create client (in separate thread to let GUI load before it connects)
-        self.scheduler_response_signal.connect(
-            self.handle_scheduler_response)
-        self.task_changed_client: GUIEventClient = GUIEventClient(
-            TaskRequest, '/task_request', self.scheduler_response_signal)
+        self.scheduler_response_signal.connect(self.handle_scheduler_response)
+        self.task_controller: GUIEventClient = GUIEventClient(
+            TaskControl, '/auto_docker_control', self.scheduler_response_signal)
 
-        # Server doesn't spin, so we init in main thread
-        self.update_task_dropdown_signal.connect(self.update_task_dropdown)
-        self.task_changed_server: GUIEventSubscriber = GUIEventSubscriber(
-            TaskFeedback, '/task_feedback', self.update_task_dropdown_signal)
 
     def start_btn_clicked(self):
         """Tell the back about the user selecting the start button."""
         # Cancel change if task changer hasn't connected yet
-        if not self.task_changed_client.connected:
+        if not self.task_controller.connected:
             return
 
-        self.task_changed_client.get_logger().info(
+        self.task_controller.get_logger().info(
             'GUI changed task to: Auto Docking')
 
-        self.task_label.setText('Task: Auto Docking')
+        self.task_status.setText('Auto Docking: Enabled')
 
-        self.task_changed_client.send_request_async(
-            TaskRequest.Request(task_id=Tasks.AUTO_DOCKING.value))
+        self.task_controller.send_request_async(
+            TaskControl.Request(start=True))
 
-    def manual_control_btn_clicked(self):
+    def stop_btn_clicked(self):
         """Tell the back about the user selecting the manual control button."""
         # Cancel change if task changer hasn't connected yet
-        if not self.task_changed_client.connected:
+        if not self.task_controller.connected:
             return
 
-        self.task_changed_client.get_logger().info(
+        self.task_controller.get_logger().info(
             'GUI changed task to: Manual Control')
 
-        self.task_label.setText('Task: Manual Control')
+        self.task_status.setText('Auto Docking: Disabled')
 
-        self.task_changed_client.send_request_async(
-            TaskRequest.Request(task_id=Tasks.MANUAL_CONTROL.value))
+        self.task_controller.send_request_async(
+            TaskControl.Request(start=False))
 
-    @ pyqtSlot(TaskRequest.Response)
-    def handle_scheduler_response(self, response: TaskRequest.Response):
+    @pyqtSlot(TaskControl.Response)
+    def handle_scheduler_response(self, response: TaskControl.Response):
         """Handle scheduler response to request sent from gui_changed_task."""
-        self.task_changed_server.get_logger().info(response.response)
-
-    # TODO @Ben do we still use this?
-    @ pyqtSlot(TaskFeedback)
-    def update_task_dropdown(self, message: TaskFeedback):
-        """Update the task selector dropdown when task changed by scheduler."""
-        self.combo_box.setCurrentIndex(message.task_id)
-        self.task_changed_server.get_logger().info(
-            f'GUI received task changed to: {self.combo_box.currentText()}' +
-            f' at {self.combo_box.currentIndex()}')
+        self.task_changed_server.get_logger().info('Auto docking is now running'
+                                                   if response.is_running else
+                                                   'Auto docking is no longer running')
