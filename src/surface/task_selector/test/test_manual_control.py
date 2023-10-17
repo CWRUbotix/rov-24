170402dad6f8d1fc54fb40f903adf55dc928c63b
Michael Carlstrom <rmc@carlstrom.com>
2023-10-17 11:01:47 -0400

    unit tests

---
+++ src/surface/task_selector/test/test_manual_control.py
@@ -0,0 +1,17 @@
+from task_selector.manual_control_node import ManualControlNode, ZERO_SPEED, RANGE_SPEED
+
+
+def test_joystick_profiles():
+    """Unit test for the joystick_profiles function"""
+
+    node = ManualControlNode()
+    # Nice boundary values
+    assert node.joystick_profiles(0) == ZERO_SPEED
+    assert node.joystick_profiles(1) == (ZERO_SPEED + RANGE_SPEED)
+    assert node.joystick_profiles(-1) == (ZERO_SPEED - RANGE_SPEED)
+
+    # Not nice possible values
+    assert node.joystick_profiles(0.34) == 1539.904
+    assert node.joystick_profiles(-0.6) == 1377.6
+
+    assert False
