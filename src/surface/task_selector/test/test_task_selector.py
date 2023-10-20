bbe22110bca17101f8cee9cddab637e2ba52ea36
Michael Carlstrom <rmc@carlstrom.com>
2023-10-20 18:06:25 -0400

    fixed blocking

--- src/surface/task_selector/test/test_task_selector.py
+++ src/surface/task_selector/test/test_task_selector.py
@@ -1,9 +1,12 @@
 from task_selector.task_selector import TaskSelector
+from task_selector.manual_control_node import main
 import rclpy
 
 
 def test_manual_control_instantiation():
     """Unit test for the Task Selector instantiation."""
     rclpy.init()
+    # TaskSelector blocks waiting for Manual Control Client
+    main()
     TaskSelector()
     rclpy.shutdown()
