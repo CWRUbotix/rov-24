866ba795e18f210e112ef3b2644ea03ddfdf768e
Michael Carlstrom <rmc@carlstrom.com>
2023-10-21 19:58:13 -0400

    remove test sadge

--- src/surface/keyboard_driver/test/test_keyboard_driver.py
+++
@@ -1,9 +0,0 @@
-import rclpy
-from keyboard_driver.keyboard_driver_node import KeyboardListenerNode
-
-
-def test_keyboard_driver_instantiation():
-    """Unit test for the Keyboard Driver instantiation."""
-    rclpy.init()
-    KeyboardListenerNode()
-    rclpy.shutdown()
