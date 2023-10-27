b3a0203855ff8d9361b046060e34aa31e79544cf
Michael Carlstrom <rmc@carlstrom.com>
2023-10-27 09:43:49 -0400

    typing

--- src/surface/task_selector/task_selector/manual_control_node.py
+++ src/surface/task_selector/task_selector/manual_control_node.py
@@ -1,4 +1,4 @@
-from array import array
+from collections.abc import MutableSequence
 
 import rclpy
 from mavros_msgs.msg import OverrideRCIn
@@ -116,8 +116,8 @@ class ManualControlNode(Node):
     def joystick_to_pixhawk(self, msg: Joy):
         rc_msg = OverrideRCIn()
 
-        axes: array[float] = msg.axes
-        buttons: array[int] = msg.buttons
+        axes: MutableSequence[float] = msg.axes
+        buttons: MutableSequence[int] = msg.buttons
 
         # DPad Pitch
         rc_msg.channels[PITCH_CHANNEL] = self.joystick_profiles(axes[DPADVERT])
@@ -169,7 +169,7 @@ class ManualControlNode(Node):
         return CancelResponse.ACCEPT
 
     def manip_callback(self, msg: Joy):
-        buttons: array[int] = msg.buttons
+        buttons: MutableSequence[int] = msg.buttons
 
         for button_id, manip_button in self.manip_buttons.items():
 
@@ -193,7 +193,7 @@ class ManualControlNode(Node):
 
     def camera_toggle(self, msg: Joy):
         """Cycles through connected cameras on pilot GUI using menu and pairing buttons."""
-        buttons: array[int] = msg.buttons
+        buttons: MutableSequence[int] = msg.buttons
 
         if buttons[MENU] == 1:
             self.seen_right_cam = True
@@ -219,5 +219,3 @@ def main():
     manual_control = ManualControlNode()
     executor = MultiThreadedExecutor()
     rclpy.spin(manual_control, executor=executor)
-    manual_control.destroy_node()
-    rclpy.shutdown()
