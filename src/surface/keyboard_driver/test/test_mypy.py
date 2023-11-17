d55d335377444ef1f31fc2a0d89928ac1abefa95
Michael Carlstrom <rmc@carlstrom.com>
2023-11-16 22:40:08 -0500

    add config file

--- src/surface/keyboard_driver/test/test_mypy.py
+++ src/surface/keyboard_driver/test/test_mypy.py
@@ -6,6 +6,7 @@ import pytest
 @pytest.mark.mypy
 @pytest.mark.linter
 def test_mypy() -> None:
-    config_file = os.path.join(__file__, '..', '..', '..', '..', 'mypy.ini')
+    file_path = __file__.replace(f'{__name__}.py', '')
+    config_file = os.path.join(file_path, '..', '..', '..', '..', 'mypy.ini')
     rc = main(argv=['--config', config_file])
     assert rc == 0, 'Found code style errors / warnings'
