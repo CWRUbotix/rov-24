913f1d4f43297b1c68f11d58fde27a1fc51b37da
Michael Carlstrom <rmc@carlstrom.com>
2023-11-16 22:25:16 -0500

    update config

--- src/pi/manipulators/test/test_mypy.py
+++ src/pi/manipulators/test/test_mypy.py
@@ -1,10 +1,11 @@
 from ament_mypy.main import main
-
+import os
 import pytest
 
 
 @pytest.mark.mypy
 @pytest.mark.linter
 def test_mypy() -> None:
-    rc = main()
+    config_file = os.path.join(__file__, '..', '..', '..', '..', 'mypy.ini')
+    rc = main(argv=['--config', config_file])
     assert rc == 0, 'Found code style errors / warnings'
