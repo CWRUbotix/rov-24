5aba87e8db04a5c47e7e2e8b591d5e492ecffe40
Michael Carlstrom <rmc@carlstrom.com>
2023-11-16 22:18:40 -0500

    fix?

--- src/surface/gui/test/test_pilot_app.py
+++ src/surface/gui/test/test_pilot_app.py
@@ -1,11 +1,11 @@
 import pytest
-from gui.pilot_app import PilotApp
+import gui
 from pytestqt.qtbot import QtBot
 
 
 @pytest.fixture
 def test_app_instantiation(qtbot: QtBot) -> None:
     """Unit test for PilotApp instantiation."""
-    app = PilotApp()
+    app = gui.operator_app.PilotApp()
     app.show()
     qtbot.addWiget(app)
