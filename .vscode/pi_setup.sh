a8638fb7436faf3b04ce3013510c7b8cef260173
Michael Carlstrom <rmc@carlstrom.com>
2023-11-18 16:09:37 -0500

    ben changes

--- .vscode/pi_setup.sh
+++ .vscode/pi_setup.sh
@@ -6,11 +6,11 @@ sudo apt-get upgrade -y
 
 # Ubuntu Universe install
 sudo apt-get install software-properties-common -y
-sudo apt-get install software-properties-common -y
 sudo add-apt-repository universe -y
 
 # Adding ROS 2 repo to system
-sudo apt-get update && sudo apt-get install curl -y
+sudo apt-get update
+sudo apt-get install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt-get update
