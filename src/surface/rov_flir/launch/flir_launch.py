64a517a7ffd1bbbbc7cd4078c42be5428e7456ec
Michael Carlstrom <rmc@carlstrom.com>
2023-11-20 16:56:22 -0500

    update flir_launch file

--- src/surface/rov_flir/launch/flir_launch.py
+++ src/surface/rov_flir/launch/flir_launch.py
@@ -4,14 +4,27 @@ from launch_ros.actions import Node
 
 def generate_launch_description() -> LaunchDescription:
 
-    # launches node to capture joystick data
-    controller_node: Node = Node(
-        package='joy',
-        executable='joy_node',
+    # launches node to run front flir camera
+    front_cam: Node = Node(
+        package='spinnaker_camera_driver',
+        executable='camera_driver_node',
         emulate_tty=True,
-        output='screen'
+        name='front_cam',
+        output='screen',
+        parameters={'serial_number': 33}
+    )
+
+    # launches node to run bottom flir camera
+    bottom_cam: Node = Node(
+        package='spinnaker_camera_driver',
+        executable='camera_driver_node',
+        emulate_tty=True,
+        name='bottom_Cam',
+        output='screen',
+        parameters={'serial_number': 34}
     )
 
     return LaunchDescription([
-        controller_node,
+        front_cam,
+        bottom_cam
     ])
