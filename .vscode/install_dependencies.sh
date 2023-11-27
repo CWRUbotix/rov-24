5582f1313bed56e82915caa925ff92589705342b
Michael Carlstrom <rmc@carlstrom.com>
2023-11-26 20:23:07 -0500

    update install files to iron

--- .vscode/install_dependencies.sh
+++ .vscode/install_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Update ROS dependencies
-. /opt/ros/humble/setup.sh && rosdep update
+. /opt/ros/iron/setup.sh && rosdep update
 sudo apt-get update -y
 
 # Installs ROS dependencies
-. /opt/ros/humble/setup.sh && rosdep install --from-paths src --ignore-src -r -y
+. /opt/ros/iron/setup.sh && rosdep install --from-paths src --ignore-src -r -y
 
 # Crazy one liner for install python dependencies
 for d in src/pi/*/ src/surface/*/; do pip install -e "$d"; done
