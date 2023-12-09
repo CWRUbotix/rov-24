d897f97f53af190613fd7b5c48401340cd0bd976
Michael Carlstrom <rmc@carlstrom.com>
2023-12-09 13:13:03 -0500

    mypy updated docs

--- src/surface/rov_flir/launch/flir_launch.py
+++ src/surface/rov_flir/launch/flir_launch.py
@@ -11,38 +11,40 @@ def generate_launch_description() -> LaunchDescription:
     parameter_file = os.path.join(get_package_share_directory('rov_flir'), 'config',
                                   'blackfly_s.yaml')
 
-    parameters = {'debug': False,
-                  'compute_brightness': False,
-                  'adjust_timestamp': True,
-                  'dump_node_map': False,
-                  # set parameters defined in blackfly_s.yaml
-                  'gain_auto': 'Continuous',
-                  # 'pixel_format': 'BayerRG8',
-                  'exposure_auto': 'Continuous',
-                  # These are useful for GigE cameras
-                  'device_link_throughput_limit': 125000000,
-                  'gev_scps_packet_size': 9000,
-                  # ---- to reduce the sensor width and shift the crop
-                  #   'image_width': 1280,
-                  #   'image_height': 720,
-                  # 'offset_x': 16,
-                  # 'offset_y': 0,
-                  'binning_x': 2,
-                  'binning_y': 2,
-                  'frame_rate_auto': 'Off',
-                  'frame_rate': 60.0,
-                  'frame_rate_enable': True,
-                  'buffer_queue_size': 1,
-                  'trigger_mode': 'Off',
-                  'chunk_mode_active': True,
-                  'chunk_selector_frame_id': 'FrameID',
-                  'chunk_enable_frame_id': True,
-                  'chunk_selector_exposure_time': 'ExposureTime',
-                  'chunk_enable_exposure_time': True,
-                  'chunk_selector_gain': 'Gain',
-                  'chunk_enable_gain': True,
-                  'chunk_selector_timestamp': 'Timestamp',
-                  'chunk_enable_timestamp': True}
+    parameters = {
+        'debug': False,
+        'compute_brightness': False,
+        'adjust_timestamp': True,
+        'dump_node_map': False,
+        # set parameters defined in blackfly_s.yaml
+        'gain_auto': 'Continuous',
+        # 'pixel_format': 'BayerRG8',
+        'exposure_auto': 'Continuous',
+        # These are useful for GigE cameras
+        'device_link_throughput_limit': 125000000,
+        'gev_scps_packet_size': 9000,
+        # ---- to reduce the sensor width and shift the crop
+        #   'image_width': 1280,
+        #   'image_height': 720,
+        # 'offset_x': 16,
+        # 'offset_y': 0,
+        'binning_x': 2,
+        'binning_y': 2,
+        'frame_rate_auto': 'Off',
+        'frame_rate': 60.0,
+        'frame_rate_enable': True,
+        'buffer_queue_size': 1,
+        'trigger_mode': 'Off',
+        'chunk_mode_active': True,
+        'chunk_selector_frame_id': 'FrameID',
+        'chunk_enable_frame_id': True,
+        'chunk_selector_exposure_time': 'ExposureTime',
+        'chunk_enable_exposure_time': True,
+        'chunk_selector_gain': 'Gain',
+        'chunk_enable_gain': True,
+        'chunk_selector_timestamp': 'Timestamp',
+        'chunk_enable_timestamp': True
+    }
 
     # launches node to run front flir camera
     front_cam: Node = Node(
