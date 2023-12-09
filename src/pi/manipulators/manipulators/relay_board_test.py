996f1a17e154cf3ad15794c21b77948f4b7f3073
Michael Carlstrom <rmc@carlstrom.com>
2023-12-09 15:22:51 -0500

    add test

--- src/pi/manipulators/manipulators/relay_board_test.py
+++ src/pi/manipulators/manipulators/relay_board_test.py
@@ -11,7 +11,6 @@ def main() -> None:
     """
     with SMBus() as bus:
         while True:
-
             bus.write_byte(TCA9555_ADDRESS, 0b00000000)
             time.sleep(1000)
             bus.write_byte(TCA9555_ADDRESS, 0b11111111)
