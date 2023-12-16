2729968d6dd76e6dfc448dc5b8114fc0c7cbb67a
NoahMollerstuen <noah@mollerstuen.com>
2023-12-16 11:56:35 -0500

    Refactored and documented custom themes

---
+++ src/surface/gui/gui/theme.py
@@ -0,0 +1,14 @@
+PROPERTY_NAME = "customStyle"
+
+
+class Style():
+    # A component is running, enabled, or armed
+    ON = "on"
+
+    # A component is disabled, not running, or disarmed, but could be enabled through this widget
+    OFF = "off"
+
+    # A component is disabled, not expected to have any effect or perform its function because of
+    # some external factor, either another widget or something external to the gui
+    # For example, a the arm button when the pi is not conncted
+    INACTIVE = "inactive"
