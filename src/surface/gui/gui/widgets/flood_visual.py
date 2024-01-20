bd12543b17b3e12e00a4185c53dc8aef97d7f315
Tyler Zupfer <tzupfer@gmail.com>
2024-01-20 16:33:23 -0500

    It's not actively erroring but it doesn't work yet

--- src/surface/gui/gui/widgets/flood_visual.py
+++ src/surface/gui/gui/widgets/flood_visual.py
@@ -1,7 +1,7 @@
-# /flooding (boolean) (True is flooding)
+# flooding (boolean) (True is flooding)
 from rov_msgs.msg import Flooding
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel
-from PyQt6.QtGui import QFont
+from PyQt6.QtGui import QFont, QColor, QPalette
 from gui.event_nodes.subscriber import GUIEventSubscriber
 from PyQt6.QtCore import pyqtSignal, pyqtSlot
 
@@ -15,29 +15,38 @@ class FloodVisual(QWidget):
         # Boilerplate PyQt Setup to link to ROS through a signal/subscriber
         self.signal.connect(self.refresh)
         self.subscription: GUIEventSubscriber = GUIEventSubscriber(Flooding,
-                                                                   '/flooding',
+                                                                   'flooding',
                                                                    self.signal,
                                                                    10)
         # Create basic 2 vertical stacked boxes layout
-        self.layout = QVBoxLayout()
+        self.layout: QVBoxLayout = QVBoxLayout()
+        self.layout.setContentsMargins(0,0,0,0)
+        self.layout.setSpacing(20)
         # Create the label that tells us what this is
-        self.label = QLabel('Flooding Indicator')
+        self.label: QLabel = QLabel('Flooding Indicator')
         # Set font and size
-        font = QFont("Arial", 14)
+        font: QFont = QFont("Arial", 14)
         self.label.setFont(font)
-
         self.layout.addWidget(self.label)
-        # Create the Indicator with a default state of green circle (green = good)
-        self.indicator = QLabel("🟢")
-        # Set font and size
-        font = QFont("Arial", 36)
-        self.indicator.setFont(font)
-
+       
+        self.indicator: Color = Color("green")
         self.layout.addWidget(self.indicator)
-
         self.setLayout(self.layout)
 
     @pyqtSlot(Flooding)
     def refresh(self, msg: Flooding):
+        palette = self.palette()
         if Flooding.flooding:
-            self.indicator.setText("🔴")
+            palette.setColor(QPalette.ColorRole.Window, QColor("red"))
+            self.indicator.setPalette(palette)
+
+
+class Color(QWidget):
+
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
