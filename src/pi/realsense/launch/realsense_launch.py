e412a5ba70b4e8b78b60792b4c7c13449a152c2f
ROV Laptop <rov@laptop.com>
2024-01-30 16:52:03 -0500

    smol

--- src/pi/realsense/launch/realsense_launch.py
+++ src/pi/realsense/launch/realsense_launch.py
@@ -30,7 +30,7 @@ def generate_launch_description() -> LaunchDescription:
         # Values are width x height x fps.
         launch_arguments={'depth_module.profile': '640x480x15',
                           'initial_reset': 'true',
-                          'rgb_camera.profile': '960x540x30'}.items()
+                          'rgb_camera.profile': '640x480x15'}.items()
     )
 
     realsense_action = GroupAction(
