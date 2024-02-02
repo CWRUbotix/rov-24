4413c3e8c1af9212a19e069dcba2a9e4a576f4d9
NoahMollerstuen <noah@mollerstuen.com>
2024-02-01 19:20:27 -0500

    Point app.py to new styles dir

--- src/surface/gui/gui/app.py
+++ src/surface/gui/gui/app.py
@@ -32,7 +32,8 @@ class App(QWidget):
         # Apply theme
         theme_param = self.theme_param.get_parameter_value().string_value
         theme_path = os.path.join(get_package_share_directory("gui"),
-                                  "themes", theme_param + ".qss")
+                                  "styles", theme_param + ".qss")
+        print(theme_path)
 
         base_theme = "dark" if theme_param == "dark" else "light"
         custom_styles = "\n"
