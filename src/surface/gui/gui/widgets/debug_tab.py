d38ba410566a1cc306900ac55211be0053d97b7f
NoahMollerstuen <noah@mollerstuen.com>
2024-02-03 16:13:23 -0500

    Add arm widget to operator gui

--- src/surface/gui/gui/widgets/debug_tab.py
+++ src/surface/gui/gui/widgets/debug_tab.py
@@ -1,5 +1,6 @@
 from gui.widgets.logger import Logger
 from gui.widgets.thruster_tester import ThrusterTester
+from gui.widgets.arm import Arm
 from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout, QWidget
 
 
@@ -7,12 +8,16 @@ class DebugWidget(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
-        root_layout = QVBoxLayout(self)
+        right_bar = QVBoxLayout()
+        right_bar.addWidget(ThrusterTester())
+        right_bar.addWidget(Arm())
+        right_bar.addStretch()
 
         top_pane = QHBoxLayout()
         top_pane.addStretch(2)
-        top_pane.addWidget(ThrusterTester())
+        top_pane.addLayout(right_bar)
 
+        root_layout = QVBoxLayout(self)
         root_layout.addLayout(top_pane)
         root_layout.addStretch()
         root_layout.addWidget(Logger())
