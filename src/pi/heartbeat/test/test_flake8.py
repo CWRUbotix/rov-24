6bc0aab229c7ee90ac6a0901977211809835042b
Michael Carlstrom <rmc@carlstrom.com>
2024-02-06 19:10:25 -0500

    test file imports

--- src/pi/heartbeat/test/test_flake8.py
+++ src/pi/heartbeat/test/test_flake8.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ament_flake8.main import main_with_errors
 import pytest
+from ament_flake8.main import main_with_errors
 
 
 @pytest.mark.flake8
