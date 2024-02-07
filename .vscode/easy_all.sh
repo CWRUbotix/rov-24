96a8bd83fe506ef738d49f47ec38487e66566165
Michael Carlstrom <rmc@carlstrom.com>
2024-02-06 22:51:37 -0500

    simple clean

--- .vscode/easy_all.sh
+++ .vscode/easy_all.sh
@@ -1,6 +1,7 @@
 # Done to suppress setup.py install deprecated warnings
 # Can be removed once ROS redoes their python build system
-PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources; export PYTHONWARNINGS
+PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources
+export PYTHONWARNINGS
 
 # Install any missing dependencies
 source "$(pwd)/.vscode/install_dependencies.sh"
--- .vscode/rov_setup.sh
+++ .vscode/rov_setup.sh
@@ -2,6 +2,6 @@
 
 # Add setup.bash to .bashrc only if it isn't already there
 ROS_LINE="source $(pwd)/install/setup.bash"
-if ! grep -qF "$ROS_LINE" ~/.bashrc ; 
-    then echo "$ROS_LINE" >> ~/.bashrc ;
+if ! grep -qF "$ROS_LINE" ~/.bashrc; then
+	echo "$ROS_LINE" >>~/.bashrc
 fi
