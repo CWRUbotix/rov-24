6717c1bff67508502957d29871d086d1aa103701
Michael Carlstrom <rmc@carlstrom.com>
2024-02-08 22:41:07 -0500

    undo test

--- src/surface/gui/gui/widgets/video_widget.py
+++ src/surface/gui/gui/widgets/video_widget.py
@@ -45,6 +45,7 @@ class CameraDescription(NamedTuple):
         The width of the Camera Stream, by default WIDTH constant.
     height: int
         The height of the Camera Stream, by default HEIGHT constant.
+
     """
 
     type: CameraType
