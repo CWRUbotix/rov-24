f424a4f5e0de20bc55fb536ba8cb941a1c1ffcdf
NoahMollerstuen <noah@mollerstuen.com>
2024-02-09 00:12:53 -0500

    Fix evil env variable ahhhh

--- src/surface/rov_gazebo/scripts/add_models_and_worlds.sh
+++ src/surface/rov_gazebo/scripts/add_models_and_worlds.sh
@@ -1,5 +1,5 @@
 # Add GZ_SIM_SYSTEM_PLUGIN_PATH to .bashrc only if it isn't already there
-ROS_LINE='export GZ_SIM_RESOURCE_PATH=~/rov-24/src/surface/rov_gazebo/models:~/rov-24/src/surface/rov_gazebo/worlds:$GZ_SIM_RESOURCE_PATH'
+ROS_LINE='export GZ_SIM_RESOURCE_PATH=$(pwd)/src/surface/rov_gazebo/models:~$(pwd)/src/surface/rov_gazebo/worlds:$GZ_SIM_RESOURCE_PATH'
 if ! grep -qF "$ROS_LINE" ~/.bashrc ; 
     then echo "$ROS_LINE" >> ~/.bashrc ;
 fi
