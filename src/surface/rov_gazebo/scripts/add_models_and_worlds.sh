acf5c074dcd2eb07d6c5724f9fa01c534a31ef58
Michael Carlstrom <rmc@carlstrom.com>
2024-02-12 19:25:22 -0500

    switch to ign sh

--- src/surface/rov_gazebo/scripts/add_models_and_worlds.sh
+++ src/surface/rov_gazebo/scripts/add_models_and_worlds.sh
@@ -1,5 +1,5 @@
-# Add GZ_SIM_SYSTEM_PLUGIN_PATH to .bashrc only if it isn't already there
-ROS_LINE='export GZ_SIM_RESOURCE_PATH=$(pwd)/src/surface/rov_gazebo/models:~$(pwd)/src/surface/rov_gazebo/worlds:$GZ_SIM_RESOURCE_PATH'
+# Add IGN_GAZEBO_SYSTEM_RESOURCE_PATH to .bashrc only if it isn't already there
+ROS_LINE='export IGN_GAZEBO_SYSTEM_RESOURCE_PATH=$(pwd)/src/surface/rov_gazebo/models:~$(pwd)/src/surface/rov_gazebo/worlds:$IGN_GAZEBO_SYSTEM_RESOURCE_PATH'
 if ! grep -qF "$ROS_LINE" ~/.bashrc ; 
     then echo "$ROS_LINE" >> ~/.bashrc ;
 fi
