2a32eb25b1a52eaef6a7efc435ba7843ad107ba0
NoahMollerstuen <noah@mollerstuen.com>
2024-02-12 19:17:25 -0500

    Switch to gz fortress, add cams WIP

--- src/surface/rov_gazebo/scripts/ardupilot_gazebo.sh
+++ src/surface/rov_gazebo/scripts/ardupilot_gazebo.sh
@@ -1,13 +1,19 @@
 sudo apt update
 sudo apt install libgz-sim7-dev rapidjson-dev
-git clone https://github.com/ArduPilot/ardupilot_gazebo ~/ardupilot_gazebo
+git clone -b fortress  https://github.com/ArduPilot/ardupilot_gazebo ~/ardupilot_gazebo
 cd ~/ardupilot_gazebo
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -j4
 
 # Add GZ_SIM_SYSTEM_PLUGIN_PATH to .bashrc only if it isn't already there
-ROS_LINE='export GZ_SIM_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:$GZ_SIM_SYSTEM_PLUGIN_PATH'
+
+ROS_LINE='export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH'
+if ! grep -qF "$ROS_LINE" ~/.bashrc ; 
+    then echo "$ROS_LINE" >> ~/.bashrc ;
+fi
+
+ROS_LINE='export IGN_GAZEBO_RESOURCE_PATH=$HOME/ardupilot_gazebo/models:$HOME/ardupilot_gazebo/worlds:IGN_GAZEBO_RESOURCE_PATH'
 if ! grep -qF "$ROS_LINE" ~/.bashrc ; 
     then echo "$ROS_LINE" >> ~/.bashrc ;
 fi
