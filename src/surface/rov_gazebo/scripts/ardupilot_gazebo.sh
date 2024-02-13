1f05ecad9a63a769173ed98727b3447ff72ed5d8
NoahMollerstuen <noah@mollerstuen.com>
2024-02-13 02:02:52 -0500

    Update arduipilot gz script

--- src/surface/rov_gazebo/scripts/ardupilot_gazebo.sh
+++ src/surface/rov_gazebo/scripts/ardupilot_gazebo.sh
@@ -2,19 +2,14 @@
 
 sudo apt-get update
 sudo apt-get install rapidjson-dev libignition-gazebo6-dev -y
-git clone -b fortress  https://github.com/ArduPilot/ardupilot_gazebo ~/ardupilot_gazebo
+git clone -b fortress  https://github.com/cwruRobotics/rov-ardupilot-gazebo-fortress ~/ardupilot_gazebo
 cd ~/ardupilot_gazebo
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -j4
 
 # Add IGN_GAZEBO_SYSTEM_PLUGIN_PATH to .bashrc only if it isn't already there
-ROS_LINE='export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/ardupilot_gazebo/build:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH'
-if ! grep -qF "$ROS_LINE" ~/.bashrc ; 
-    then echo "$ROS_LINE" >> ~/.bashrc ;
-fi
-
-ROS_LINE='export IGN_GAZEBO_RESOURCE_PATH=$HOME/ardupilot_gazebo/models:$HOME/ardupilot_gazebo/worlds:IGN_GAZEBO_RESOURCE_PATH'
+ROS_LINE='export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=$HOME/rov-ardupilot-gazebo-fortress/build:$IGN_GAZEBO_SYSTEM_PLUGIN_PATH'
 if ! grep -qF "$ROS_LINE" ~/.bashrc ; 
     then echo "$ROS_LINE" >> ~/.bashrc ;
 fi
