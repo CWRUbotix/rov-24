423d0065343583dc11a3b41d733c422ac2194800
Michael Carlstrom <rmc@carlstrom.com>
2024-02-12 19:44:08 -0500

    add todo

--- src/surface/rov_gazebo/scripts/add_models_and_worlds.sh
+++ src/surface/rov_gazebo/scripts/add_models_and_worlds.sh
@@ -1,4 +1,5 @@
 # Add IGN_GAZEBO_SYSTEM_RESOURCE_PATH to .bashrc only if it isn't already there
+# TODO FIX pwd not being resolved
 ROS_LINE='export IGN_GAZEBO_SYSTEM_RESOURCE_PATH=$(pwd)/src/surface/rov_gazebo/models:~$(pwd)/src/surface/rov_gazebo/worlds:$IGN_GAZEBO_SYSTEM_RESOURCE_PATH'
 if ! grep -qF "$ROS_LINE" ~/.bashrc ; 
     then echo "$ROS_LINE" >> ~/.bashrc ;
