ce8e1bf6cfcb6102c73d3b2dc517c1474eb70d4e
Michael Carlstrom <rmc@carlstrom.com>
2024-02-13 23:51:36 -0500

    move deps

--- src/surface/flight_control/setup.py
+++ src/surface/flight_control/setup.py
@@ -17,7 +17,7 @@ setup(
         (os.path.join('share', PACKAGE_NAME, 'launch'),
          glob('launch/*launch.[pxy][yma]*'))
     ],
-    install_requires=['setuptools', 'flake8==4.0.1', 'mypy>=1.7', 'pynput'],
+    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='benjamin',
     maintainer_email='bwp18@case.edu',
