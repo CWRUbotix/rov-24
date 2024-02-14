8429c847052dbdcce7988b5332f33ad9a213f65f
Michael Carlstrom <rmc@carlstrom.com>
2024-02-14 15:14:42 -0500

    flake8 test success

--- src/pi/manipulators/test/test_mypy.py
+++ src/pi/manipulators/test/test_mypy.py
@@ -9,5 +9,4 @@ from ament_mypy.main import main
 def test_mypy() -> None:
     """Tests mypy on this module."""
     error_code = main(argv=[])
-    hi = "ttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt"
     assert error_code == 0, "Found code style errors / warnings"
