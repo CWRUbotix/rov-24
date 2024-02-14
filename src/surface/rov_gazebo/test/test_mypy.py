e4d1ae6984ecdf61dbe1fc6847e39ea342300163
Michael Carlstrom <rmc@carlstrom.com>
2024-02-14 00:10:33 -0500

    fix mypy

--- src/surface/rov_gazebo/test/test_mypy.py
+++ src/surface/rov_gazebo/test/test_mypy.py
@@ -1,5 +1,4 @@
 """Test mypy on this module."""
-import os
 
 import pytest
 from ament_mypy.main import main
@@ -9,7 +8,5 @@ from ament_mypy.main import main
 @pytest.mark.linter
 def test_mypy() -> None:
     """Tests mypy on this module."""
-    file_path = __file__.replace(f'{__name__}.py', '')
-    config_file = os.path.join(file_path, '..', '..', '..', '..', 'mypy.ini')
-    error_code = main(argv=['--config', config_file])
+    error_code = main(argv=[])
     assert error_code == 0, 'Found code style errors / warnings'
