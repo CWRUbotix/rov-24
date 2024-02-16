0606d761e5da8b890156e84c38e3089ec29631ec
rov-rhubarb <rov-rhubarb@cwrubotics.com>
2024-02-15 19:11:47 -0500

    merging main

--- src/pi/flood_detection/flood_detection/gpio_reader.py
+++ src/pi/flood_detection/flood_detection/gpio_reader.py
@@ -6,15 +6,15 @@ import lgpio
 
 class floodDetector(Node):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__('gpio_reader', parameter_overrides=[])
         self.publisher_ = self.create_publisher(Flooding, '/tether/flooding', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
-    def timer_callback(self):
+    def timer_callback(self) -> None:
         # Pins used for GPIO
-        detect1 = 17
+        detect1: int = 17
         # GPIO Boilerplate
         h = lgpio.gpiochip_open(0)
         lgpio.gpio_claim_input(h, detect1)
@@ -35,9 +35,9 @@ class floodDetector(Node):
         self.get_logger().info('Publishing: "%s"' % msg.flooding)
 
 
-def main(args=None):
+def main(args: None = None) -> None:
     rclpy.init()
-    flood_detector = floodDetector()
+    flood_detector: floodDetector = floodDetector()
     rclpy.spin(flood_detector)
     flood_detector.destroy_node()
     rclpy.shutdown()
