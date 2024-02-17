6aa7863d00a2bbd0e05b2335087f7fccd282d208
Michael Carlstrom <rmc@carlstrom.com>
2024-02-17 15:34:19 -0500

    test

--- src/surface/gui/launch/pilot_launch.py
+++ src/surface/gui/launch/pilot_launch.py
@@ -16,7 +16,7 @@ def generate_launch_description() -> LaunchDescription:
                     ("/surface/gui/front_cam/image_raw", "/surface/front_cam/image_raw"),
                     ("/surface/gui/depth_cam/image_raw", "/tether/depth_cam/image_raw"),
                     ("/surface/gui/vehicle_state_event", "/surface/vehicle_state_event"),
-                    ("/surface/gui/flooding", "/tether/flooding")],
+                    ("/surface/gui/flooding", "/tether/flooding")], 
         emulate_tty=True,
         output='screen'
     )
