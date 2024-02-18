0a58a7f3e449a4a73a48f7aff25186ff75350f37
NoahMollerstuen <noah@mollerstuen.com>
2024-02-18 00:56:43 -0500

    Refactor timer and gui nodes

--- src/surface/gui/setup.py
+++ src/surface/gui/setup.py
@@ -9,8 +9,7 @@ PACKAGE_NAME = 'gui'
 setup(
     name=PACKAGE_NAME,
     version='1.1.0',
-    packages=[PACKAGE_NAME, os.path.join(PACKAGE_NAME, 'widgets'),
-              os.path.join(PACKAGE_NAME, 'event_nodes')],
+    packages=[PACKAGE_NAME, os.path.join(PACKAGE_NAME, 'widgets')],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + PACKAGE_NAME]),
@@ -34,6 +33,6 @@ setup(
     entry_points={
         'console_scripts': ['run_pilot = gui.pilot_app:run_gui_pilot',
                             'run_operator = gui.operator_app:run_gui_operator',
-                            'run_timer = gui.auxiliary_nodes.timer:run_timer'],
+                            'run_timer = gui.gui_nodes.auxiliary_nodes.timer:run_timer'],
     },
 )
