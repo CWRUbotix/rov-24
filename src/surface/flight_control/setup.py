376ac829482eb8aea28346b85e0d957d681c1a45
rickyrnt <rick.yarnot.255@gmail.com>
2024-03-23 19:29:45 +0000

    Fix setup script

--- src/surface/flight_control/setup.py
+++ src/surface/flight_control/setup.py
@@ -28,7 +28,8 @@ setup(
         'console_scripts': [
             'manual_control_node = flight_control.manual_control_node:main',
             'keyboard_control_node = flight_control.keyboard_control_node:main',
-            'auto_docking_node = flight_control.auto_docking_node:main'
+            'auto_docking_node = flight_control.auto_docking_node:main',
+            'control_inverter_node = flight_control.control_inverter_node:main'
         ],
     },
 )
