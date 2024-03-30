6952c24ca873d0f0106e5049e6b68b7d4fd895e7
rov-rhubarb <rov-rhubarb@cwrubotics.com>
2024-03-30 15:56:16 -0400

    flood detector finally?

--- src/pi/flood_detection/flood_detection/flood_detector.py
+++ src/pi/flood_detection/flood_detection/flood_detector.py
@@ -16,7 +16,7 @@ class floodDetector(Node):
                                                '/tether/flooding',
                                                10)
         time.sleep(2)
-        self.gpio_chip = lgpio.gpiochip_open(0)
+        self.gpio_chip = lgpio.gpiochip_open(0)c
         lgpio.gpio_claim_input(self.gpio_chip, DETECT_PIN)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -27,12 +27,12 @@ class floodDetector(Node):
 
         # If any of the sensors detect water, send true to /tether/flooding
         msg = Flooding()
-      
+
         if data1:
             msg.flooding = True
         else:
             msg.flooding = False
-        self.publisher_.publish(msg)
+        self.publisher.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.flooding)
 
 
