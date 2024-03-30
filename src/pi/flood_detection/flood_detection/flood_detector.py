3a45bb9b391ad4714f43b456f1d238df3ac24202
Tyler Zupfer <tzupfer@gmail.com>
2024-03-30 13:07:21 -0400

    fixing things?

--- src/pi/flood_detection/flood_detection/flood_detector.py
+++ src/pi/flood_detection/flood_detection/flood_detector.py
@@ -2,31 +2,31 @@ import rclpy
 from rclpy.node import Node
 from rov_msgs.msg import Flooding
 import lgpio
+import time
 
+# Pins used for GPIO
+DETECT_PIN: int = 17
 
 class floodDetector(Node):
 
-    def __init__(self) -> None:
+    def __init__(self) -> None:      
         super().__init__('gpio_reader', parameter_overrides=[])
-        self.publisher_ = self.create_publisher(Flooding, '/tether/flooding', 10)
+        self.publisher = self.create_publisher(Flooding,
+                                                '/tether/flooding',
+                                                10)
+        time.sleep(2)
+        self.gpio_chip = lgpio.gpiochip_open(0)
+        lgpio.gpio_claim_input(self.gpio_chip, DETECT_PIN)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self) -> None:
-        # Pins used for GPIO
-        detect1: int = 17
-        # GPIO Boilerplate
-        h = lgpio.gpiochip_open(0)
-        lgpio.gpio_claim_input(h, detect1)
-
-        data1: bool = lgpio.gpio_read(h, detect1)
-
-        print("Pin 17: %s" % data1)
+        data1: bool = lgpio.gpio_read(self.gpio_chip, DETECT_PIN)
+        self.get_logger().info("Pin 17: %s" % data1)
 
         # If any of the sensors detect water, send true to /tether/flooding
-
         msg = Flooding()
-
+        
         if data1:
             msg.flooding = True
         else:
