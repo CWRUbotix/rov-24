250d41b1f5f292fffea247977e2aa34105a1be16
Shannon Griswold <shannon.v.griswold@gmail.com>
2024-04-06 15:53:48 -0400

    Removed an unused import

--- src/pi/pi_main/launch/pi_launch.py
+++ src/pi/pi_main/launch/pi_launch.py
@@ -5,7 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch.launch_description import LaunchDescription
 from launch.actions import GroupAction, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import PushRosNamespace, Node
+from launch_ros.actions import PushRosNamespace
 
 
 def generate_launch_description() -> LaunchDescription:
