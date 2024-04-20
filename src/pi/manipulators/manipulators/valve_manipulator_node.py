949dad4b6e28253e8c6d1a51d2991f471ab5acb9
rov-rhubarb <rov-rhubarb@cwrubotics.com>
2024-04-20 11:12:42 -0400

    code?

--- src/pi/manipulators/manipulators/valve_manipulator_node.py
+++ src/pi/manipulators/manipulators/valve_manipulator_node.py
@@ -1,35 +1,48 @@
 import lgpio
-import time
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import qos_profile_system_default
+from rov_msgs.msg import Manip
 
 # Configuration
-SERVO_PIN = 12  # pin used to drive PWM fan
-FREQ = 50
-
-gpio_handle = lgpio.gpiochip_open(0)
+SERVO_PIN = 12  # pin used to drive Valve Manip
 
 
-def test_gpio(width: int, freq: int) -> None:
-    lgpio.tx_servo(gpio_handle, SERVO_PIN, width, freq)
+class ValveManipulator(Node):
+    def __init__(self) -> None:
+        super().__init__('valve_manipulator')
+        self.create_subscription(
+            Manip,
+            'manipulator_control',
+            self.manip_callback,
+            qos_profile_system_default
+        )
+
+        self.gpio_handle = lgpio.gpiochip_open(0)
+
+    def servo(self, width: int, freq: int = 50) -> None:
+        lgpio.tx_servo(self.gpio_handle, SERVO_PIN, width, freq)
+
+    def manip_callback(self, message: Manip) -> None:
+        if message.activated:
+            self.servo(2500)
+        else:
+            self.servo(0)
 
 
-try:
-    print('Starting loop')
-    while True:
-        # Turn the fan off
-        test_gpio(2000, 50)
-        time.sleep(2)
+def main() -> None:
+    rclpy.init()
 
-        print('on')
-        # Turn the fan to medium speed
-        test_gpio(1000, 200)
-        time.sleep(2)
+    subscriber = ValveManipulator()
 
-        print('more on')
-        # Turn the fan to max speed
-        test_gpio(500, 400)
-        time.sleep(2)
+    rclpy.spin(subscriber)
 
-except KeyboardInterrupt:
-    # Turn the fan to medium speed
-    lgpio.tx_pwm(gpio_handle, SERVO_PIN, FREQ, 50)
-    lgpio.gpiochip_close(gpio_handle)
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
