b76be1b9ce075b0cd100a541e8bc0f6ae6432519
Michael Carlstrom <rmc@carlstrom.com>
2024-04-27 15:36:32 -0400

    fix ben sadness

--- src/float/test/test_cpplint.py
+++ src/float/test/test_cpplint.py
@@ -29,12 +29,12 @@ def test_cpplint() -> None:
         # Copies folders
         shutil.copytree(os.path.join(os.getcwd(), folder), os.path.join(TMP, folder))
         # Renames files
-        os.rename(os.path.join(TMP, folder, folder + INO),
-                  os.path.join(TMP, folder, folder + CPP))
+        os.rename(os.path.join(TMP, folder, folder + INO_EXTENSION),
+                  os.path.join(TMP, folder, folder + CPP_EXTENSION))
 
     # regular_cpp = [os.path.join(os.getcwd(), folder) for folder in [SRC, INCLUDE]]
     regular_cpp = []
-    regular_cpp.extend([os.path.join(TMP, folder, folder + CPP) for folder in SKETCHES])
+    regular_cpp.extend([os.path.join(TMP, folder, folder + CPP_EXTENSION) for folder in SKETCHES])
     for file in regular_cpp:
         error_code = main(argv=["paths", file])
         assert error_code == 0, 'Found code style errors / warnings'
