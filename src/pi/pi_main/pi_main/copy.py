2524104d3b78b58dd15adb9272aed935b6b646e1
Michael Carlstrom <rmc@carlstrom.com>
2024-04-29 17:22:29 -0400

    update

--- src/pi/pi_main/pi_main/copy.py
+++ src/pi/pi_main/pi_main/copy.py
@@ -10,4 +10,9 @@ if __name__ == '__main__':
     udev_dst_dir = os.path.join('/etc', 'udev', 'rules.d')
 
     shutil.copytree(udev_src_dir, udev_dst_dir, dirs_exist_ok=True)
+
+    service_src = os.path.join(SHARE_DIR, 'services' 'pi_main.service')
+    service_dst = os.path.join('/etc', 'systemmd', 'systemd', 'pi_main.service')
+
+    shutil.copy(service_src, service_dst)
     print("Copying udev rules.")
