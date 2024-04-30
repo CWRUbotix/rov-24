834fdc4985c0e369dbe643bcd98da97ee9306431
ROV Laptop <rov@laptop.com>
2024-04-29 22:05:23 -0400

    bigger video

--- src/surface/gui/gui/pilot_app.py
+++ src/surface/gui/gui/pilot_app.py
@@ -5,7 +5,7 @@ from gui.widgets.flood_warning import FloodWarning
 from gui.widgets.video_widget import (CameraDescription, CameraType,
                                       SwitchableVideoWidget, VideoWidget)
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout
+from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout, QBoxLayout
 
 
 class PilotApp(App):
@@ -26,38 +26,42 @@ class PilotApp(App):
             front_cam_type = CameraType.ETHERNET
             bottom_cam_type = CameraType.ETHERNET
             depth_cam_type = CameraType.DEPTH
-        # TODO Look into QStackedLayout for possibly switching between
+
+        if gui_param.value == 'pilot':
+            title = 'Pilot GUI - CWRUbotix ROV 2024'
+
+            video_layout: QHBoxLayout | QBoxLayout = main_layout
+            front_cam_size = [1280, 720]
+            bottom_cam_size = front_cam_size
+            depth_cam_size = [640, 360]
+        else:
+            title = 'Debug GUI - CWRUbotix ROV 2024'
+
+            video_layout = QHBoxLayout()
+            main_layout.addLayout(video_layout)
+            front_cam_size = [721, 541]
+            bottom_cam_size = front_cam_size
+            depth_cam_size = [640, 360]
+
         # 1 big camera feed and 2 smaller ones
         front_cam_description = CameraDescription(front_cam_type,
                                                   'front_cam/image_raw',
-                                                  'Front Camera')
-
-        main_video = VideoWidget(front_cam_description)
+                                                  'Front Camera', *front_cam_size)
 
         bottom_cam_description = CameraDescription(bottom_cam_type,
                                                    'bottom_cam/image_raw',
-                                                   'Bottom Camera')
+                                                   'Bottom Camera', *bottom_cam_size)
         depth_cam_description = CameraDescription(depth_cam_type,
                                                   'depth_cam/image_raw',
-                                                  'Depth Camera', 640, 360)
+                                                  'Depth Camera', *depth_cam_size)
 
+        main_video = VideoWidget(front_cam_description)
         video_area = SwitchableVideoWidget([bottom_cam_description, depth_cam_description],
                                            "camera_switch")
 
-        if gui_param.value == 'pilot':
-            self.setWindowTitle('Pilot GUI - CWRUbotix ROV 2024')
-
-            main_layout.addWidget(main_video, alignment=Qt.AlignmentFlag.AlignHCenter)
-            main_layout.addWidget(video_area, alignment=Qt.AlignmentFlag.AlignHCenter)
-        else:
-            self.setWindowTitle('Debug GUI - CWRUbotix ROV 2024')
-
-            video_layout = QHBoxLayout()
-
-            video_layout.addWidget(main_video, alignment=Qt.AlignmentFlag.AlignHCenter)
-            video_layout.addWidget(video_area, alignment=Qt.AlignmentFlag.AlignHCenter)
-
-            main_layout.addLayout(video_layout)
+        self.setWindowTitle(title)
+        video_layout.addWidget(main_video, alignment=Qt.AlignmentFlag.AlignHCenter)
+        video_layout.addWidget(video_area, alignment=Qt.AlignmentFlag.AlignHCenter)
 
         bottom_screen_layout = QHBoxLayout()
 
