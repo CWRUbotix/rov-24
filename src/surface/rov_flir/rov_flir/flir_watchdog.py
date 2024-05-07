2fc31ca4a164bd2362fe548fd12978c51ae5dda7
ROV Laptop <rov@laptop.com>
2024-05-07 10:37:59 -0400

    Add watchdog to reboot camera drivers

---
+++ src/surface/rov_flir/rov_flir/flir_watchdog.py
@@ -0,0 +1,69 @@
+import rclpy
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.node import Node
+from subprocess import Popen
+import atexit
+
+from typing import List, Optional
+
+WATCHDOG_RATE = 10
+NAMESPACE = "surface"
+
+
+class Watchdog():
+    def __init__(self, node: Node, args: List[str]) -> None:
+        self.node: Node = node
+        self.args = args
+        self.process: Optional[Popen[bytes]] = None
+
+        self.start_process()
+
+    def start_process(self) -> None:
+        self.process = Popen(self.args)
+
+    def poll(self) -> None:
+        if self.process is not None and self.process.poll() is not None:
+            self.node.get_logger().warning("Detected camera crash, restarting...")
+            self.start_process()
+
+    def kill(self) -> None:
+        if self.process:
+            self.process.kill()
+
+
+class FlirWatchdogNode(Node):
+    def __init__(self) -> None:
+        super().__init__("flir_watchdog_node", parameter_overrides=[])
+
+        self.timer = self.create_timer(1 / WATCHDOG_RATE, self.timer_callback)
+
+        self.get_logger().info("Starting camera drivers")
+
+        self.front_watchdog = Watchdog(
+            node=self,
+            args=["ros2", "launch", "rov_flir", "flir_launch.py",
+                  "launch_bottom:=false", f"ns:={NAMESPACE}"],
+        )
+        self.bottom_watchdog = Watchdog(
+            node=self,
+            args=["ros2", "launch", "rov_flir", "flir_launch.py",
+                  "launch_front:=false", f"ns:={NAMESPACE}"],
+        )
+
+        atexit.register(self.front_watchdog.kill)
+        atexit.register(self.bottom_watchdog.kill)
+
+    def timer_callback(self) -> None:
+        self.front_watchdog.poll()
+        self.bottom_watchdog.poll()
+
+
+def main() -> None:
+    rclpy.init()
+    flir_watchdog_node = FlirWatchdogNode()
+    executor = MultiThreadedExecutor()
+    rclpy.spin(flir_watchdog_node, executor=executor)
+
+
+if __name__ == "__main__":
+    main()
--- src/surface/surface_main/launch/surface_pilot_launch.py
+++ src/surface/surface_main/launch/surface_pilot_launch.py
@@ -4,7 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch.launch_description import LaunchDescription
 from launch.actions import GroupAction, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import PushRosNamespace
+from launch_ros.actions import PushRosNamespace, Node
 from launch.conditions import UnlessCondition
 from launch.substitutions import LaunchConfiguration
 
@@ -13,7 +13,7 @@ def generate_launch_description() -> LaunchDescription:
 
     gui_path: str = get_package_share_directory('gui')
     controller_path: str = get_package_share_directory('ps5_controller')
-    flir_path: str = get_package_share_directory('rov_flir')
+    # flir_path: str = get_package_share_directory('rov_flir')
 
     simulation_configuration = LaunchConfiguration('simulation', default=False)
 
@@ -36,12 +36,20 @@ def generate_launch_description() -> LaunchDescription:
     )
 
     # Launches flir
-    flir_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                flir_path, 'launch', 'flir_launch.py'
-            )
-        ]),
+    # flir_launch = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource([
+    #         os.path.join(
+    #             flir_path, 'launch', 'flir_launch.py'
+    #         )
+    #     ]),
+    #     condition=UnlessCondition(simulation_configuration)
+    # )
+    flir_launch = Node(
+        package='rov_flir',
+        executable='flir_watchdog',
+        name='flir_watchdog',
+        emulate_tty=True,
+        output='screen',
         condition=UnlessCondition(simulation_configuration)
     )
 
