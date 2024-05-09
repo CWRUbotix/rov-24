d0457d63c81689dc812f5b7ad0c3c4312fca85a3
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2024-05-09 17:26:48 -0400

    Remake links script

---
+++ src/float/remake_links.sh
@@ -0,0 +1,6 @@
+rm surface_transceiver/rov_common.cpp surface_transceiver/rov_common.hpp float_transceiver/rov_common.cpp float_transceiver/rov_common.hpp
+
+ln include/rov_common.hpp surface_transceiver/rov_common.hpp
+ln include/rov_common.hpp float_transceiver/rov_common.hpp
+ln include/rov_common.cpp surface_transceiver/rov_common.cpp
+ln include/rov_common.cpp float_transceiver/rov_common.cpp
