ed8e05e11d9ac1801ca533acc34a43cfa49a8edc
NoahMollerstuen <noah@mollerstuen.com>
2024-06-09 14:57:59 -0400

    Refactor and add docstrings

--- src/surface/gui/gui/pilot_app.py
+++ src/surface/gui/gui/pilot_app.py
@@ -27,23 +27,6 @@ TWO_MONITOR_CONFIG: dict[GuiType, int | None] = {GuiType.PILOT: None, GuiType.LI
 THREE_MONITOR_CONFIG: dict[GuiType, int | None] = {GuiType.PILOT: 2, GuiType.LIVESTREAM: 1}
 
 
-def make_bottom_bar() -> QHBoxLayout:
-    bottom_screen_layout = QHBoxLayout()
-
-    timer = TimerDisplay()
-    bottom_screen_layout.addWidget(timer)
-
-    flood_widget = FloodWarning()
-    bottom_screen_layout.addWidget(flood_widget, alignment=Qt.AlignmentFlag.AlignHCenter |
-                                   Qt.AlignmentFlag.AlignBottom)
-
-    arm = Arm()
-    bottom_screen_layout.addWidget(arm, alignment=Qt.AlignmentFlag.AlignRight |
-                                   Qt.AlignmentFlag.AlignBottom)
-
-    return bottom_screen_layout
-
-
 class PilotApp(App):
     def __init__(self) -> None:
         super().__init__('pilot_gui_node')
@@ -86,7 +69,7 @@ class PilotApp(App):
                 alignment=Qt.AlignmentFlag.AlignHCenter
             )
 
-            main_layout.addLayout(make_bottom_bar())
+            main_layout.addLayout(self.make_bottom_bar())
 
         elif gui_type == GuiType.LIVESTREAM:
             top_bar = QHBoxLayout()
@@ -150,11 +133,45 @@ class PilotApp(App):
             )
 
             main_layout.addLayout(video_layout)
-            main_layout.addLayout(make_bottom_bar())
+            main_layout.addLayout(self.make_bottom_bar())
 
         self.apply_monitor_config(gui_type)
 
+    def make_bottom_bar() -> QHBoxLayout:
+        """Generate a bottom pane used by multiple gui types
+
+        Returns
+        -------
+        QHBoxLayout
+            The layout containing the bottom bar widgets
+        """
+        bottom_screen_layout = QHBoxLayout()
+
+        timer = TimerDisplay()
+        bottom_screen_layout.addWidget(timer)
+
+        flood_widget = FloodWarning()
+        bottom_screen_layout.addWidget(flood_widget, alignment=Qt.AlignmentFlag.AlignHCenter |
+                                       Qt.AlignmentFlag.AlignBottom)
+
+        arm = Arm()
+        bottom_screen_layout.addWidget(arm, alignment=Qt.AlignmentFlag.AlignRight |
+                                       Qt.AlignmentFlag.AlignBottom)
+
+        return bottom_screen_layout
+
     def apply_monitor_config(self, gui_type: GuiType) -> None:
+        """Fullscreen the app to a specific monitor, depending on gui_type and the monitor config
+        Either fullscreens the app to a monitor specified by TWO_MONITOR_CONFIG or
+        THREE_MONITOR_CONFIG (depending on the number of monitors present), or does nothing if no
+        config exists for the number of monitors and gui type
+
+        Parameters
+        ----------
+        gui_type : GuiType
+            The type of gui that is being initialized
+        """
+
         screen = self.screen()
         if screen is None:
             return
